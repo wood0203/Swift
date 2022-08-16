5e1ba232928544faf082847347029e13bb46ecab
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2022-08-16 23:30:53 +0900

    a

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by 1 on 2022/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
