6beef8c071eb5129ab37ab04de2ee2efa2d6773d
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2022-08-28 15:03:30 +0900

    v

---
+++ Project/RescueFinder2/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  RescueFinder
+//
+//  Created by 1 on 2022/08/23.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <DaumMap/MTMapView.h>
+#import <DaumMap/MTMapReverseGeoCoder.h>
+
+#endif /* Header_h */
